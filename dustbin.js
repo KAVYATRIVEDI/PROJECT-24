c443566c2d0557d0b034140169471e4627c2c5a1
KAVYATRIVEDI <69847315+KAVYATRIVEDI@users.noreply.github.com>
2020-09-16 21:51:39 +0530

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,32 @@
+class DUSTBIN {
+    constructor(x, y) {
+     
+        var options={
+            instatic : false,
+            restitution:0.3,
+            friction:2.5,
+            density:1.2
+        }
+        
+      this.body = Bodies.rectangle(x, y, 50, 50 ,options);
+      this.width = 50;
+      this.height = 50;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      pos.x=mouseX ;
+      pos.y= mouseY;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      strokeWeight(4);
+      stroke("green")
+      fill(255);
+      rect(0, 0, this.width, this.height);
+      pop();
+    }
+  };
---
+++ sketch.js
@@ -0,0 +1,42 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	dustbin= new Dustbin(100,100);
+
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+
+dustbin.display();
+  
+  drawSprites();
+ 
+}
+
+
+
